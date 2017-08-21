ce9dedb187233f97c5713fc18546944edbfefee7
Karthick <karthi.vels@gmail.com>
2017-08-21 13:44:16 +0530

    added recycler listener

---
+++ custom-ui/src/main/java/com/thapovan/android/customui/recyclerview/RecyclerClickListener.java
@@ -0,0 +1,7 @@
+package com.thapovan.android.customui.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+
+public interface RecyclerClickListener {
+    public void onViewItemClick(int position, RecyclerView.ViewHolder viewHolder);
+}
---
+++ custom-ui/src/main/java/com/thapovan/android/customui/recyclerview/RecyclerViewSpaceDecoration.java
@@ -0,0 +1,37 @@
+package com.thapovan.android.customui.recyclerview;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+
+public class RecyclerViewSpaceDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public RecyclerViewSpaceDecoration(int space) {
+        this.space = space;
+    }
+
+    public RecyclerViewSpaceDecoration(Context context){
+        this.space = dipToPixels(context,15f);
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.left = space;
+        outRect.right = space;
+        outRect.bottom = space;
+
+        // Add top margin only for the first item to avoid double space between items
+        if(parent.getChildAdapterPosition(view) == 0){
+            outRect.top = space;
+        }
+    }
+
+    private int dipToPixels(Context context, float dipValue) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, metrics);
+    }
+}
