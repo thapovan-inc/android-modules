eba59bc8f18b80e4d50deecdc5e126520e2f3a30
Karthick <karthi.vels@gmail.com>
2017-08-22 12:47:27 +0530

    added ScrollingFABBehavior

---
+++ common-utils/src/main/java/com/thapovan/android/commonutils/app/AppUtil.java
@@ -0,0 +1,18 @@
+package com.thapovan.android.commonutils.app;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+import com.thapovan.android.commonutils.R;
+
+public class AppUtil {
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+}
---
+++ common-utils/src/main/java/com/thapovan/android/commonutils/internetcheck/NetworkStateReceiver.java
@@ -0,0 +1,72 @@
+package com.thapovan.android.commonutils.internetcheck;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkStateReceiver extends BroadcastReceiver {
+
+    private static final String LOG_TAG = NetworkStateReceiver.class.getSimpleName();
+
+    protected List<NetworkStateReceiverListener> listeners;
+    protected Boolean connected;
+
+    public NetworkStateReceiver() {
+        listeners = new ArrayList<>();
+        connected = null;
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        if(intent == null)
+            return;
+
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = manager.getActiveNetworkInfo();
+
+        if(ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        } else if(intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
+            connected = false;
+        }
+
+        Log.d(LOG_TAG,"Connection Changed connected "+connected);
+
+        notifyStateToAll();
+
+    }
+
+    private void notifyStateToAll() {
+        for(NetworkStateReceiverListener listener : listeners)
+            notifyState(listener);
+    }
+
+    private void notifyState(NetworkStateReceiverListener listener) {
+        if(connected == null || listener == null)
+            return;
+
+        if(connected)
+            listener.networkAvailable();
+        else
+            listener.networkUnavailable();
+    }
+
+    public void addListener(NetworkStateReceiverListener l) {
+        listeners.add(l);
+        notifyState(l);
+    }
+
+    public void removeListener(NetworkStateReceiverListener l) {
+        listeners.remove(l);
+    }
+
+    public interface NetworkStateReceiverListener {
+        void networkAvailable();
+        void networkUnavailable();
+    }
+}
