f52bd77109b7107db0a4b18613465b401e13f6b1
Karthick <karthi.vels@gmail.com>
2017-08-22 18:02:41 +0530

    implementing facebook login cleanUp

--- app/src/main/java/com/thapovan/android/FacebookLoginActivity.java
+++ app/src/main/java/com/thapovan/android/FacebookLoginActivity.java
@@ -3,25 +3,60 @@ package com.thapovan.android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
-import com.thapovan.android.commonutils.toast.ToastUtil;
 import com.thapovan.android.socialnetwork.facebook.FacebookLogin;
 import com.thapovan.android.socialnetwork.facebook.model.FacebookProfile;
 import com.thapovan.android.socialnetwork.facebook.subscriber.FacebookEventSubscriber;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class FacebookLoginActivity extends AppCompatActivity implements FacebookEventSubscriber {
 
+    @BindView(R.id.buttonFBLogin)           Button buttonFBLogin;
+    @BindView(R.id.buttonFBLogout)          Button buttonFBLogout;
+    @BindView(R.id.textName)                TextView textName;
+    @BindView(R.id.textEmail)               TextView textEmail;
+    @BindView(R.id.imageProfile)            ImageView imageProfile;
+
+    FacebookProfile profile = null;
+    FacebookLoginActivity mActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mActivity = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
         ButterKnife.bind(this);
-        FacebookLogin.getInstance().initFacebookLogin(getApplicationContext());
+        FacebookLogin.getInstance().initFacebookLogin(mActivity);
+
+        refreshUI();
+    }
+
+    private void refreshUI(){
+        if(FacebookLogin.getInstance().isUserSignedIn()){
+            buttonFBLogin.setVisibility(View.GONE);
+            buttonFBLogout.setVisibility(View.VISIBLE);
+            FacebookLogin.getInstance().getUserProfile(mActivity);
+        }else {
+            buttonFBLogin.setVisibility(View.VISIBLE);
+            buttonFBLogout.setVisibility(View.GONE);
+        }
+
+        if(profile != null){
+            textName.setText(profile.getName());
+            textEmail.setText(profile.getEmail());
+        }else {
+            textName.setText("");
+            textEmail.setText("");
+        }
     }
 
     @Override
@@ -32,28 +67,32 @@ public class FacebookLoginActivity extends AppCompatActivity implements Facebook
 
     @OnClick(R.id.buttonFBLogin)
     public void onButtonFBLogin() {
-        FacebookLogin.getInstance().onFBClicked(this);
-//        FacebookLogin.getInstance().getUserProfile();
+        FacebookLogin.getInstance().onLoginClicked(mActivity);
+    }
+
+    @OnClick(R.id.buttonFBLogout)
+    public void onButtonFBLogout() {
+        FacebookLogin.getInstance().onLogoutClicked();
     }
 
     @Override
     public void onFacebookLoginSuccess(LoginResult loginResult) {
-        ToastUtil.showToast(this,"loginResult "+loginResult.getAccessToken().getToken() );
-                FacebookLogin.getInstance().getUserProfile(this);
+        FacebookLogin.getInstance().getUserProfile(mActivity);
     }
 
     @Override
     public void onFacebookLoginCancel() {
-        ToastUtil.showToast(this,"onFacebookLoginCancel ");
+
     }
 
     @Override
     public void onFacebookLoginError(FacebookException error) {
-        ToastUtil.showToast(this,"onFacebookLoginError ");
+
     }
 
     @Override
-    public void onFacebookProfileGen(FacebookProfile profile) {
-        ToastUtil.showToast(this,"profile "+profile.getName());
+    public void onFacebookProfileGet(FacebookProfile profile) {
+        this.profile = profile;
+        refreshUI();
     }
 }
